55ce227bb153cfbf5b5accd2331584d01b1eeb76
nxivm-bots <imvoid1006@gmail.com>
2025-01-04 00:52:40 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://openmessage?user_id={OWNER_ID}'>L Λ W L I Ξ T</a>\n○ Channel : @Nxivm_Anime\n○ Support Group : @Nxivm_Support</b>",
+            text = f"<b>○ Creator : <a href='tg://openmessage?user_id={OWNER_ID}'>S Λ I N T</a>\n○ Channel : @Nxivm_Anime\n○ Support Group : @Nxivm_Support</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
